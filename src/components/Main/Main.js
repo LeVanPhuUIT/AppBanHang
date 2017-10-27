8ba6edee80b7369615b60ca1f1326614e0baf777
LeVanPhuUIT <phulevan1995@gmail.com>
2017-10-27 22:24:26 +0700

    Designing model of the application
    install yarn
    install react-native-drawer
    install react-native-tab-navigator
    install model menu and link to  the screens

---
+++ src/components/Main/Main.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+//import { View, Text} from 'react-native';
+import Drawer from 'react-native-drawer';
+
+import Menu from './Menu';
+import Shop from './Shop/Shop';
+
+export default class Main extends Component {
+  gotoAuthentication() {
+    const { navigator } = this.props;
+    navigator.push({ name: 'AUTHENTICATION' });
+  }
+  gotoChangeInfo() {
+    const { navigator } = this.props;
+    navigator.push({ name: 'CHANGE_INFO' });
+  }
+  gotoOrderHistory() {
+    const { navigator } = this.props;
+    navigator.push({ name: 'ORDER_HISTORY' });
+  }
+  closeControlPanel = () => {
+    this.drawer.close();
+  };
+  openControlPanel = () => {
+    this.drawer.open();
+  };
+
+  render() {
+    const { navigator } = this.props;
+    return (
+        ///kéo qua kéo lại
+      <Drawer
+        ref={ref => {
+          this.drawer = ref;
+        }}
+        content={<Menu navigator={navigator} />}
+        tapToClose
+        openDrawerOffset={0.4}
+      >
+        <Shop open={this.openControlPanel.bind(this)} />
+      </Drawer>
+    );
+  }
+}
