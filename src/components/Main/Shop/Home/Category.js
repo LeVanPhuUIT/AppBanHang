d371a48c7bd7ebfad15d6d680ab77b7360ebd3df
LeVanPhuUIT <phulevan1995@gmail.com>
2017-11-08 12:39:31 +0700

    add on home screen
    Install: react-native-swiper
    Add: Category.js, Collection.js
    Edit: home.js

---
+++ src/components/Main/Shop/Home/Category.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  Dimensions,
+  TouchableOpacity,
+} from 'react-native';
+import Swiper from 'react-native-swiper';
+
+import littleIcon from '../../../../media/temp/little.jpg';
+import maxiIcon from '../../../../media/temp/maxi.jpg';
+import partyIcon from '../../../../media/temp/party.jpg';
+
+const { width, height } = Dimensions.get('window');
+
+export default class Category extends Component {
+    render() {
+        const { wrapper, textStyle, imageStyle } = styles;
+        return (
+          <View style={wrapper}>
+            <View style={{ flex: 1, justifyContent: 'center' }}>
+              <Text style={textStyle}> SPRING COLLECTION</Text>
+            </View>
+            <View style={{ flex: 4 }}>
+              <Swiper >
+                <Image source={littleIcon} style={imageStyle} />
+                <Image source={maxiIcon} style={imageStyle} />
+                <Image source={partyIcon} style={imageStyle} />
+              </Swiper>
+            </View>
+          </View>
+        );
+    }
+}
+//933 x 465
+const imageWidth = width - 40;
+const imageHeight = imageWidth / 2;
+
+const styles = StyleSheet.create({
+    wrapper: {
+        height: height * 0.33,
+        backgroundColor: '#FFF',
+        margin: 10,
+        justifyContent: 'space-between',
+        shadowColor: '#2E272B',
+        shadowOffset: { width: 0, height: 3 },
+        shadowOpacity: 0.2,
+        padding: 10,
+        paddingTop: 0
+    },
+    textStyle: {
+        fontSize: 20,
+        color: '#AFAEAF'
+    },
+    imageStyle: {
+        height: imageHeight,
+        width: imageWidth,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    cateTitle: {
+        fontSize: 20,
+        fontFamily: 'Avenir',
+        color: '#9A9A9A'
+    }
+});
--- src/components/Main/Shop/Home/home.js
+++ src/components/Main/Shop/Home/home.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
+import Collection from './Collection';
+import Category from './Category';
 class Home extends Component {
     render() {
         return (
-            <View style={{ flex: 1, backgroundColor: 'red' }}> 
-                <Text>Home</Text>
+            <View style={{ flex: 1, backgroundColor: '#DBDBD8' }}> 
+                <Collection />
+                <Category />
             </View>
         );
     }
