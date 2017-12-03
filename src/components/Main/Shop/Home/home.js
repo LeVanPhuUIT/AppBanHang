9d732b66d6a321d48bccad4ff95241e7ff02d276
NguyenMinhTri <nmtri.uit@gmail.com>
2017-12-03 16:09:50 +0700

    Add Home screen

--- src/components/Main/Shop/Home/home.js
+++ src/components/Main/Shop/Home/home.js
@@ -1,20 +1,39 @@
 import React, { Component } from 'react';
-import { ScrollView, Text } from 'react-native';
+import { Navigator } from 'react-native';
 
-import Collection from './Collection';
-import Category from './Category';
-import TopProduct from './TopProduct';
+import HomeView from './HomeView';
+import ProductDetail from '../ProductDetail/ProductDetail';
+import ListProduct from '../ListProduct/ListProduct';
 
 class Home extends Component {
-    render() {
-        return (
-            <ScrollView style={{ flex: 1, backgroundColor: '#DBDBD8' }}> 
-                <Collection />
-                <Category />
-                <TopProduct />
-            </ScrollView>
-        );
-    }
+  render() {
+    const { types, topProducts } = this.props;
+    return (
+      <Navigator
+        initialRoute={{ name: 'HOME_VIEW' }}
+        renderScene={(route, navigator) => {
+          switch (route.name) {
+            case 'HOME_VIEW':
+              return (
+                <HomeView
+                  navigator={navigator}
+                  types={types}
+                  topProducts={topProducts}
+                />
+              );
+            case 'LIST_PRODUCT':
+              return (
+                <ListProduct navigator={navigator} category={route.category} />
+              );
+            default:
+              return (
+                <ProductDetail navigator={navigator} product={route.product} />
+              );
+          }
+        }}
+      />
+    );
+  }
 }
 
 export default Home;
