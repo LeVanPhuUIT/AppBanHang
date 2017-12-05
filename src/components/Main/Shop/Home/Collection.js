7c69b6f20c4ac89a256f86e1fca036381b5ad1f8
LeVanPhuUIT <phulevan1995@gmail.com>
2017-12-05 18:15:20 +0700

    Add funtion get data from server for Collection

--- src/components/Main/Shop/Home/Collection.js
+++ src/components/Main/Shop/Home/Collection.js
@@ -12,30 +12,33 @@ import bannerImage from '../../../../media/temp/banner.jpg';
 const { width } = Dimensions.get('window');
 
 export default class Collection extends Component {
-    gotoListProduct() {
-        const { navigator } = this.props;
-        navigator.push({ name: 'LIST_PRODUCT' });
-    }
-    render() {
-        const { wrapper, textStyle, imageStyle } = styles;
-        return (
-            <View style={wrapper} >
-                <View style={{ height: 50, justifyContent: 'center' }}>
-                    <Text style={textStyle}>SPRING COLLECTION</Text>
-                </View>
-                <TouchableOpacity
-                ///style={{ flex: 4, justifyContent: 'flex-end' }}
-                onPress={this.gotoListProduct.bind(this)}
-                >
-                <Image source={bannerImage} style={imageStyle} />
-                </TouchableOpacity>
-            </View>
-        );
-    }
+  gotoListProduct() {
+    const { navigator } = this.props;
+    navigator.push({
+      name: 'LIST_PRODUCT',
+      category: { name: 'SPRING COLLECTION', id: 'COLLECTION' }
+    });
+  }
+  render() {
+    const { wrapper, textStyle, imageStyle } = styles;
+    return (
+      <View style={wrapper}>
+        <View style={{ height: 50, justifyContent: 'center' }}>
+          <Text style={textStyle}>SPRING COLLECTION</Text>
+        </View>
+        <TouchableOpacity
+          style={{ flex: 4, justifyContent: 'flex-end' }}
+          onPress={this.gotoListProduct.bind(this)}
+        >
+          <Image source={bannerImage} style={imageStyle} />
+        </TouchableOpacity>
+      </View>
+    );
+  }
 }
-
+//933 x 465
 const imageWidth = width - 40;
-const imageHeight = (imageWidth / 933) * 465;
+const imageHeight = imageWidth / 933 * 465;
 
 const styles = StyleSheet.create({
   wrapper: {
