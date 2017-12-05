b644d71496555945e890c43ef15265a239c24bd1
LeVanPhuUIT <phulevan1995@gmail.com>
2017-12-06 00:10:14 +0700

    Add funtion get data from server for Login and Logout

--- src/components/Authentication/SignIn.js
+++ src/components/Authentication/SignIn.js
@@ -1,61 +1,94 @@
 import React, { Component } from 'react';
-import { View, TextInput, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import {
+  View,
+  TextInput,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  Alert
+} from 'react-native';
+import signIn from '../../api/signIn';
+import global from '../global';
+
+import saveToken from '../../api/saveToken';
 
 export default class SignIn extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            email: '',
-            password: ''
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      test: '0'
+    };
+  }
 
+  //SAI_THONG_TIN_DANG_NHAP
+    onFail() {
+    Alert.alert(
+      'Notice',
+      'Email or Password is incorrect',
+      [{ text: 'OK' }],
+      { cancelable: false }
+    );
+  }
 
-    render() {
-        const { inputStyle, bigButton, buttonText } = styles;
-        const { email, password } = this.state;
-        return (
-            <View>
-                <TextInput
-                    style={inputStyle}
-                    placeholder="Enter your email"
-                    value={email}
-                    onChangeText={text => this.setState({ email: text })}
-                />
-                <TextInput
-                    style={inputStyle}
-                    placeholder="Enter your password"
-                    value={password}
-                    onChangeText={text => this.setState({ password: text })}
-                    secureTextEntry
-                />
-                <TouchableOpacity style={bigButton} >
-                    <Text style={buttonText}>SIGN IN NOW</Text>
-                </TouchableOpacity>
-            </View>
-        );
-    }
+  onSignIn() {
+    const { email, password } = this.state;
+    signIn(email, password)
+      .then(res => {
+        this.test = res.text;
+        global.onSignIn(res.user);
+        this.props.goBackToMain();
+        saveToken(res.token);
+      })
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    const { inputStyle, bigButton, buttonText } = styles;
+    const { email, password } = this.state;
+    return (
+      <View>
+        <TextInput
+          style={inputStyle}
+          placeholder="Enter your email"
+          value={email}
+          onChangeText={text => this.setState({ email: text })}
+        />
+        <TextInput
+          style={inputStyle}
+          placeholder="Enter your password"
+          value={password}
+          onChangeText={text => this.setState({ password: text })}
+          secureTextEntry
+        />
+        <TouchableOpacity style={bigButton} onPress={this.onSignIn.bind(this)}>
+          <Text style={buttonText}>SIGN IN NOW</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    inputStyle: {
-        height: 50,
-        backgroundColor: '#fff',
-        marginBottom: 10,
-        borderRadius: 20,
-        paddingLeft: 30
-    },
-    bigButton: {
-        height: 50,
-        borderRadius: 20,
-        borderWidth: 1,
-        borderColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    buttonText: {
-        fontFamily: 'Avenir',
-        color: '#fff',
-        fontWeight: '400'
-    }
+  inputStyle: {
+    height: 50,
+    backgroundColor: '#fff',
+    marginBottom: 10,
+    borderRadius: 20,
+    paddingLeft: 30
+  },
+  bigButton: {
+    height: 50,
+    borderRadius: 20,
+    borderWidth: 1,
+    borderColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  buttonText: {
+    fontFamily: 'Avenir',
+    color: '#fff',
+    fontWeight: '400'
+  }
 });
